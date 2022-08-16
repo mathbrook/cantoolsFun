18ca86543e2eaebf407277b39bc4677de3ab75bf
mathewos samson <mathos.brook@gmail.com>
2022-08-15 20:16:30 -0400

    updated shell&python script

--- candumpks5e.sh
+++ candumpks5e.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
-lsusb
+sudo dmesg | grep tty
 read -p "what port is the candapter on " usbPort
-sudo slcand -o -s8 -t hw -S 1000000 ttyUSB$usbPort
+sudo slcand -o -s7 -t hw -S 1000000 ttyUSB$usbPort
 sudo ip link set up slcan0
 candump slcan0 | python3 -m cantools decode ks5e.dbc
--- ks5eCanParser.py
+++ ks5eCanParser.py
@@ -7,7 +7,8 @@ dbc.add_dbc_file('20200701_RMS_PM_CAN_DB.dbc')
 cantools.database.dump_file(dbc,"ks5e.dbc")
 print(dbc)
 ks5edbc = cantools.database.load_file('ks5e.dbc')
-#can_bus = can.interface.Bus('slcan0',bustype='socketcan')
-# while(True):
-#     message=can_bus.recv()
-#     ks5edbc.decode_message(message.arbitration_id,message.data)
+ks5edbc.refresh()
+can_bus = can.interface.Bus('vcan0',bustype='socketcan')
+while(True):
+    message=can_bus.recv()
+    print(ks5edbc.decode_message(message.arbitration_id,message.data))
